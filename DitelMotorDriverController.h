9d1e49554fa38b32dc1b1136dba9eb843b6290db
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2025-04-12 16:38:37 +0900

    ✨ outputリミットの変更50→800 12Vで385を回すため

--- DitelMotorDriverController.h
+++ DitelMotorDriverController.h
@@ -8,7 +8,7 @@
 #define DITEL_MOTOR_NEUTRAL 0x12
 #define DITEL_MOTOR_BRAKE 0x13
 #define DITEL_NONE 0
-#define DITEL_MOTOR_MAX_OUTPUT 50
+#define DITEL_MOTOR_MAX_OUTPUT 800
 
 int DitelMotorDriverRotate(CAN_HandleTypeDef *hcan, uint8_t _motorDriverAddress, uint8_t _mode, uint16_t output);
 int DitelMotor(CAN_HandleTypeDef *hcan, int motor_address, int output);
